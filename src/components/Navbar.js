966691390ad5775ac7aebefaee015af450269e24
ParulSingh16 <116558560+ParulSingh16@users.noreply.github.com>
2022-12-02 10:10:05 +0000

    Completed navbar dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,24 +12,36 @@ const Navbar = () => (
         </Link>
       </div>
       <div className="navbar-end">
-        <Link to="/makeups/lipstick" className="navbar-item">
-          Lipsticks
-        </Link>
-        <Link to="/makeups/nailpaint" className="navbar-item">
-          NailPaints
-        </Link>
-        <Link to="/makeups/foundation" className="navbar-item">
-          Foundations
-        </Link>
-        <Link to="/makeups/blush" className="navbar-item">
-          Blush
-        </Link>
-        <Link to="/makeups/mascara" className="navbar-item">
-          Mascara
-        </Link>
-        <Link to="/makeups/eyebrow" className="navbar-item">
-          Eyebrow
-        </Link>
+        <nav
+          className="navbar is-dark"
+          role="navigation"
+          aria-label="dropdown navigation"
+        >
+          <div className="navbar-item has-dropdown is-hoverable">
+            <div className="navbar-item has-text-white">Explore Further</div>
+
+            <div className="navbar-dropdown">
+              <Link to="/makeups/lipstick" className="navbar-item">
+                Lipsticks
+              </Link>
+              <Link to="/makeups/nailpaint" className="navbar-item">
+                NailPaints
+              </Link>
+              <Link to="/makeups/foundation" className="navbar-item">
+                Foundations
+              </Link>
+              <Link to="/makeups/blush" className="navbar-item">
+                Blush
+              </Link>
+              <Link to="/makeups/mascara" className="navbar-item">
+                Mascara
+              </Link>
+              <Link to="/makeups/eyebrow" className="navbar-item">
+                Eyebrow
+              </Link>
+            </div>
+          </div>
+        </nav>
       </div>
     </div>
   </nav>
