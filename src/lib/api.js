21b7a798b9bf61c8a4240e89262daba08ecc23ad
ermaitland <maitland.eleanor@gmail.com>
2023-01-29 10:14:37 +0000

    deploying

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const MAKEUP_ENDPOINT = 'http://makeup-api.herokuapp.com/api/v1/products.json';
+const MAKEUP_ENDPOINT = 'https://makeup-api.herokuapp.com/api/v1/products.json';
 
 export const getAllMakeup = () => axios.get(MAKEUP_ENDPOINT);
 
